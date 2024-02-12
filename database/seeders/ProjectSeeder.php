24c04b584a4e8911821b97b3a89b365b9f39b25d
lordevito <lollo.devito@hotmail.com>
2024-02-12 01:39:23 +0100

    feat:aggiunta type_id

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Project;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use App\Models\Type;
 
 class ProjectSeeder extends Seeder
 {
@@ -15,15 +16,19 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        for ($i = 0; $i < 30; $i++) {
+        Project::truncate();
+        for ($i = 0; $i < 40; $i++) {
+
+            $type = Type::inRandomOrder()->first();
+
             $newproject = new Project();
 
             $newproject->title = $faker->sentence(7);
             $newproject->description = $faker->text(500);
             $newproject->slug = Str::of($newproject->title)->slug('-');
             $newproject->languages = $faker->randomElement(['HTML, CSS e JS', 'PHP']);
-            $newproject->frameworks = $faker->randomElement(['Laravel', 'React', 'Vue', 'Angular', 'Bootstrap']);
-
+            $newproject->frameworks = $faker->randomElement(['Laravel', 'Lang', 'Vue', 'Angular', 'Bootstrap']);
+            $newproject->type_id = $type->id;
             $newproject->save();
         }
     }
